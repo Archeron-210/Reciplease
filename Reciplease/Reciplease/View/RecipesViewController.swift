cdff396738a6bdbfc0f445b9a1d7294be5b0d44a
Laura Jacquin <laura.jcqn@live.fr>
2021-12-29 17:43:03 +0100

    started working on backbone of search interface

---
+++ Reciplease/Reciplease/View/RecipesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RecipesViewController.swift
+//  Reciplease
+//
+//  Created by Archeron on 29/12/2021.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.reloadData()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension RecipesViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
+
+        // remplir les cellules
+
+        return cell
+    }
+}
