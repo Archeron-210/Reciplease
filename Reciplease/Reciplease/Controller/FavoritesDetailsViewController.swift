5290ded4e290418eb8b22f8c56e2acb59754c637
Laura Jacquin <laura.jcqn@live.fr>
2022-01-03 16:58:47 +0100

    added outlets and actions on controllers

--- Reciplease/Reciplease/Controller/FavoritesDetailsViewController.swift
+++ Reciplease/Reciplease/Controller/FavoritesDetailsViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class FavoritesDetailsViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var recipePicture: UIImageView!
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var ingredientListTextView: UITextView!
+    @IBOutlet weak var getDirectionButton: UIButton!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var likesAndTimeStackView: UIStackView!
+
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setInterfaceAspect()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func getDirectionsButtonTapped(_ sender: UIButton) {
     }
-    */
+    @IBAction func favoriteButtonTapped(_ sender: Any) {
+        updateFavoriteIcon()
+    }
 
+    // MARK: - Private functions
+
+    private func updateFavoriteIcon() {
+        if !favoriteButton.isSelected {
+            favoriteButton.setImage(UIImage(systemName: "star.fill"), for: .selected)
+        } else {
+            favoriteButton.setImage(UIImage(systemName: "star"), for: .normal)
+        }
+    }
+
+    // MARK: - UI Aspect
+
+    private func setInterfaceAspect() {
+        getDirectionButton.layer.cornerRadius = 10.0
+        likesAndTimeStackView.layer.cornerRadius = 5.0
+        likesAndTimeStackView.layer.borderWidth = 0.5
+        likesAndTimeStackView.layer.borderColor = UIColor.white.cgColor
+    }
 }
--- Reciplease/Reciplease/View/RecipeDetailViewController.swift
+++ Reciplease/Reciplease/View/RecipeDetailViewController.swift
@@ -1,29 +1,68 @@
-//
-//  RecipeDetailViewController.swift
-//  Reciplease
-//
-//  Created by Archeron on 29/12/2021.
-//
+
 
 import UIKit
 
 class RecipeDetailViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var recipePicture: UIImageView!
+    @IBOutlet weak var likesAndTimeStackView: UIStackView!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var recipeTitleLabel: UILabel!
+    @IBOutlet weak var ingredientListTextView: UITextView!
+    @IBOutlet weak var getDirectionsButton: UIButton!
+    @IBOutlet weak var favoriteButton: UIButton!
+
+
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setInterfaceAspect()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func getDirectionsButtonTapped(_ sender: UIButton) {
     }
-    */
+    @IBAction func favoriteButtonTapped(_ sender: UIButton) {
+        updateFavoriteIcon()
+    }
 
+    // MARK: - Private functions
+
+    private func updateLikesAndTimeLabels() {
+
+    }
+
+    private func updateRecipePicture() {
+
+    }
+
+    private func updateRecipeTitleLable() {
+
+    }
+
+    private func updateIngredientList() {
+
+    }
+
+    private func updateFavoriteIcon() {
+        if !favoriteButton.isSelected {
+            favoriteButton.setImage(UIImage(systemName: "star.fill"), for: .selected)
+        } else {
+            favoriteButton.setImage(UIImage(systemName: "star"), for: .normal)
+        }
+
+    }
+
+
+    // MARK: - UI Aspect
+
+    private func setInterfaceAspect() {
+        getDirectionsButton.layer.cornerRadius = 10.0
+        likesAndTimeStackView.layer.cornerRadius = 5.0
+        likesAndTimeStackView.layer.borderWidth = 0.5
+        likesAndTimeStackView.layer.borderColor = UIColor.white.cgColor
+    }
 }
